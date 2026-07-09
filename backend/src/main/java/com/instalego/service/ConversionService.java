9e487f0727cca29d4472602e3d399185657b872e
shiva <vetvenkatashiva2@gmail.com>
2026-07-09 23:49:23 +0530

    Fix circular reference: add @Lazy to ConversionService self-injection

--- backend/src/main/java/com/instalego/service/ConversionService.java
+++ backend/src/main/java/com/instalego/service/ConversionService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ public class ConversionService {
     private final ObjectMapper objectMapper;
 
     @Autowired
+    @Lazy
     private ConversionService self;
 
     @Value("${app.upload-dir}")
